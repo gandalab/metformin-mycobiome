c2fc5a2fe973b58d5fed3c23863b2e9464dff7bc
Emily Van Syoc <epb5360@psu.edu>
2023-04-04 11:16:29 -0400

    Add files via upload

---
+++ R/T2DNOMETvNORM/fido-bmi.R
@@ -0,0 +1,297 @@
+### FIDO - covariates - by paper
+
+## BMI
+
+# EVS  2/2023
+
+library(fido)
+library(tidyverse)
+library(phyloseq)
+library(ggpubr)
+library(microViz)
+library(rstatix)
+
+# get myFIDO function
+source("R/myFIDO.R")
+
+## get OTU table
+load("data/fungi-phyloseq-filtered.RData")
+
+psf <- psf %>% tax_glom("Genus")
+taxa_names(psf) <- tax_table(psf)[,"Genus"]
+
+# how many papers have BMI metrics?
+psf %>% samdat_tbl() %>% 
+  group_by(paper) %>% 
+  get_summary_stats(bmi.kg.m2, type = "mean_sd")
+# forslund, karlsson, lechatlier, li, qin, sun, elbere2020, nielsen
+
+### get only taxa that are shared between all papers
+shared <- psf %>% ps_filter(!paper %in% c("wu2017")) %>% merge_samples(group = "paper") %>% 
+  tax_transform("pa") %>% 
+  tax_filter(min_prevalence = 8)
+
+# filter original ps by shared taxa
+psh <- prune_taxa(taxa_names(shared), psf)
+
+## ---- global test ----
+
+# NO PAIRED SAMPLES - use only baseline values from RCTs (only provided one set of clinical vars - not long enough to see differences in BMI)
+ps <- psh
+
+metadata <- sample_data(psh)
+df <- data.frame(metadata) %>% 
+  drop_na(bmi.kg.m2) %>% 
+  # remove post-metformin samples from RCTs (sun and elbere)
+  mutate(papermet = paste0(paper, metformin)) %>% 
+  filter(!papermet %in% c("sun2018metformin")) %>% 
+  filter(!papermet %in% c("elbere2020metformin")) %>% 
+  # standardize
+  
+  mutate(bmi.stand = (bmi.kg.m2 - mean(bmi.kg.m2) ) / sd(bmi.kg.m2)) %>% 
+  mutate(metformin = if_else(metformin == "metformin", 1, 0))
+
+# get summary stats
+df %>% get_summary_stats(bmi.kg.m2, type = "common")
+
+# get otu tabl
+otu <- otu_table(ps)
+
+## make model matrix
+X <- t(model.matrix(~ bmi.stand + metformin + paper, data = df))
+
+# set Y
+Y <- otu[,colnames(otu) %in% colnames(X)]
+
+# Specify priors
+upsilon <- ntaxa(ps)+3 
+Omega <- diag(ntaxa(ps))
+G <- cbind(diag(ntaxa(ps)-1), -1)
+Xi <- (upsilon-ntaxa(ps))*G%*%Omega%*%t(G)
+Theta <- matrix(0, ntaxa(ps)-1, nrow(X))
+Gamma <- diag(nrow(X))
+
+## prior checking
+priors <- pibble(NULL, X, upsilon, Theta, Gamma, Xi)  
+#print(priors)
+
+## convert to CLR
+priors <- to_clr(priors)  
+
+# set names
+names_covariates(priors) <- rownames(X)
+
+priors$Y <- Y
+
+# refit with priors to get posterior
+posterior <- refit(priors, optim_method="lbfgs", n_samples = 5000)
+
+# convert to CLR
+posterior <- to_clr(posterior)
+
+# name the posterior categories (taxa)
+names_categories(posterior) <- taxa_names(ps)
+params = summary(posterior, pars = "Lambda")
+
+# get sigs
+sig.params <- params$Lambda %>% 
+  filter(covariate == "bmi.stand") %>% 
+  filter(sign(p2.5) == sign(p97.5)) # 5
+
+# save results
+bmipar <- params
+save(bmipar, file = "results/fido-bmi-params.RData")
+
+# get r2
+quantile(r2(posterior, covariate = 2),
+         probs = c(0.025, 0.5, 0.975)
+)
+
+
+## ----- BY PAPER ----
+
+### FORSLUND 2015
+### NO PAIRED SAMPLES
+
+## subset metadata and OTU table
+ps <- psf %>% 
+  ps_filter(paper == "forslund2015") 
+
+df <- data.frame(metadata) %>% 
+  filter(paper == "forslund2015") %>% 
+  # drop NAs
+  drop_na(bmi.kg.m2) %>% 
+  # standardize fbg
+  mutate(bmi.stand = (bmi.kg.m2 - mean(bmi.kg.m2) ) / sd(bmi.kg.m2)) %>% 
+  dplyr::select(bmi.stand)
+
+## run 
+fors <- myFIDO(ps, df, covariates.include = 2)
+
+### KARLSSON
+
+## subset metadata and OTU table
+ps <- psf %>% 
+  ps_filter(paper == "karlsson2013") 
+
+df <- data.frame(metadata) %>% 
+  filter(paper == "karlsson2013") %>% 
+  drop_na(bmi.kg.m2) %>% 
+  # standardize fbg
+  mutate(bmi.stand = (bmi.kg.m2 - mean(bmi.kg.m2) ) / sd(bmi.kg.m2)) %>% 
+  dplyr::select(metformin, bmi.stand)
+
+karl <- myFIDO(ps, df, get_r2 = FALSE)
+
+### LECHATLIER 
+
+ps <- psf %>% 
+  ps_filter(paper == "lechatlier2013") 
+
+df <- data.frame(metadata) %>% 
+  filter(paper == "lechatlier2013") %>% 
+  drop_na(bmi.kg.m2) %>% 
+  # standardize fbg
+  mutate(bmi.stand = (bmi.kg.m2 - mean(bmi.kg.m2) ) / sd(bmi.kg.m2)) %>% 
+  dplyr::select(bmi.stand)
+
+lech <- myFIDO(ps, df, get_r2 = FALSE)
+
+### LI 2014
+
+ps <- psf %>% 
+  ps_filter(paper == "li2014") 
+
+df <- data.frame(metadata) %>% 
+  filter(paper == "li2014") %>% 
+  drop_na(bmi.kg.m2) %>% 
+  # standardize fbg
+  mutate(bmi.stand = (bmi.kg.m2 - mean(bmi.kg.m2) ) / sd(bmi.kg.m2)) %>% 
+  dplyr::select(bmi.stand, metformin)
+
+li <- myFIDO(ps, df, get_r2 = FALSE)
+
+
+### QIN 2012
+ps <- psf %>% 
+  ps_filter(paper == "qin2012") 
+
+df <- data.frame(metadata) %>% 
+  filter(paper == "qin2012") %>% 
+  drop_na(bmi.kg.m2) %>% 
+  # standardize fbg
+  mutate(bmi.stand = (bmi.kg.m2 - mean(bmi.kg.m2) ) / sd(bmi.kg.m2)) %>% 
+  dplyr::select(bmi.stand, metformin)
+
+qin <- myFIDO(ps, df, get_r2 = FALSE)
+
+## SUN 2018 
+
+ps <- psf %>% 
+  ps_filter(paper == "sun2018") 
+
+df <- data.frame(metadata) %>% 
+  filter(paper == "sun2018") %>% 
+  ### GET ONLY BEFORE METFORMIN 
+  filter(metformin == "no.metformin") %>% 
+  drop_na(bmi.kg.m2) %>% 
+  # standardize fbg
+  mutate(bmi.stand = (bmi.kg.m2 - mean(bmi.kg.m2) ) / sd(bmi.kg.m2)) %>% 
+  dplyr::select(bmi.stand)
+
+sun <- myFIDO(ps, df, get_r2 = FALSE)
+
+### ELBERE 2020
+ps <- psf %>% 
+  ps_filter(paper == "elbere2020") 
+
+df <- data.frame(metadata) %>% 
+  filter(paper == "elbere2020") %>% 
+  ### GET ONLY BEFORE METFORMIN 
+  filter(metformin == "no.metformin") %>% 
+  drop_na(bmi.kg.m2) %>% 
+  # standardize fbg
+  mutate(bmi.stand = (bmi.kg.m2 - mean(bmi.kg.m2) ) / sd(bmi.kg.m2)) %>% 
+  dplyr::select(bmi.stand)
+
+elb <- myFIDO(ps, df, get_r2 = FALSE)
+
+## NIELSEN 2014
+ps <- psf %>% 
+  ps_filter(paper == "nielsen2014") 
+
+df <- data.frame(metadata) %>% 
+  filter(paper == "nielsen2014") %>% 
+  drop_na(bmi.kg.m2) %>% 
+  # standardize fbg
+  mutate(bmi.stand = (bmi.kg.m2 - mean(bmi.kg.m2) ) / sd(bmi.kg.m2)) %>% 
+  dplyr::select(bmi.stand)
+
+ni <- myFIDO(ps, df, get_r2 = FALSE)
+
+
+
+## ---- save all ----
+
+## add together
+all_pars <- rbind(fors %>% mutate(Study = "Forslund 2014"),
+                  karl %>% mutate(Study = "Karlsson 2013"),
+                  lech %>% mutate(Study = "LeChatlier 2013"),
+                  li %>% mutate(Study = "Li 2014"),
+                  qin %>% mutate(Study = "Qin 2012"),
+                  sun %>% mutate(Study = "Sun 2018"),
+                  elb %>% mutate(Study = "Elbere 2020"),
+                  ni %>% mutate(Study = "Nielsen 2014"))
+
+save(all_pars, file = "results/fido-bmi-bypaper-params.RData")
+
+## ---- FILTER ----
+
+
+## the significant coords from global comparison
+sigs <- sig.params$coord # 5
+
+## filter the full dataset to only significant taxa
+bypaper <- all_pars %>% 
+  filter(coord %in% sigs) %>% 
+  # get only insulin
+  filter(covariate == "bmi.stand") %>% 
+  mutate(Genus = str_remove(coord, "clr_"))
+
+### THRESHOLD: there were 8 studies in the comparison,
+# so consider significant if 5 have same direction and have magnitude over 0.1
+
+
+taxon <- unique(bypaper$Genus)
+outdf <- data.frame()
+for(i in 1:length(taxon)) {
+  
+  # subset
+  sub <- bypaper %>% filter(Genus == taxon[i])
+  outdf[i, "Genus"] <- taxon[i]
+  
+  # summarize effect size direction (sign of mean)
+  
+  if(length(which(sign(sub$mean) == 1)) >= 5 |
+     length(which(sign(sub$mean) == -1)) >= 5) {
+    
+    # create new column in outdf
+    outdf[i, "signs"] <- TRUE
+  } else { outdf[i, "signs"] <- FALSE}
+  
+  
+  # summarize strength of effect size
+  if(length(which(abs(sub$mean) > 0.1)) >= 5) {
+    
+    outdf[i, "size"] <- TRUE
+  } else { outdf[i, "size"] <- FALSE}
+  
+}
+
+outdf
+
+## save taxa for new plot
+mod1 <- outdf %>% 
+  filter(signs == TRUE & size == TRUE) # 2 of which Ustilago remains if threshold is set to 6
+
+
---
+++ R/T2DNOMETvNORM/fido-glucose.R
@@ -0,0 +1,265 @@
+### FIDO - covariates - by paper
+
+## fasting blood glucose
+
+# EVS  2/2023
+
+library(fido)
+library(tidyverse)
+library(phyloseq)
+library(ggpubr)
+library(microViz)
+library(rstatix)
+
+# get myFIDO function
+source("R/myFIDO.R")
+
+## get OTU table
+load("data/fungi-phyloseq-filtered.RData")
+
+psf <- psf %>% tax_glom("Genus")
+taxa_names(psf) <- tax_table(psf)[,"Genus"]
+
+# how many papers have glucose metrics?
+psf %>% samdat_tbl() %>% 
+  group_by(paper) %>% 
+  get_summary_stats(fasting.glucose.mg.dL, type = "mean_sd")
+# forslund, karlsson, lechatlier, li, qin, sun
+
+
+### get only taxa that are shared between all papers
+shared <- psf %>% ps_filter(!paper %in% c("wu2017", "nielsen2014", "elbere2020")) %>% merge_samples(group = "paper") %>% 
+  tax_transform("pa") %>% 
+  tax_filter(min_prevalence = 6)
+
+# filter original ps by shared taxa
+psh <- prune_taxa(taxa_names(shared), psf)
+
+## ---- global test ----
+
+# NO PAIRED SAMPLES - use only baseline values from RCTs (only provided one set of clinical vars - not long enough to see differences in BMI)
+ps <- psh
+
+metadata <- sample_data(psh)
+df <- data.frame(metadata) %>% 
+  drop_na(fasting.glucose.mg.dL) %>% 
+  # remove post-metformin samples from RCTs (sun and elbere)
+  mutate(papermet = paste0(paper, metformin)) %>% 
+  filter(!papermet %in% c("sun2018metformin")) %>% 
+  # standardize
+  
+  mutate(fbg.stand = (fasting.glucose.mg.dL - mean(fasting.glucose.mg.dL) ) / sd(fasting.glucose.mg.dL)) %>% 
+  mutate(metformin = if_else(metformin == "metformin", 1, 0))
+
+## get summary stats for results
+df %>% get_summary_stats(fasting.glucose.mg.dL, type = "common")
+
+# get otu tabl
+otu <- otu_table(ps)
+
+## make model matrix
+X <- t(model.matrix(~ fbg.stand + metformin + paper, data = df))
+
+# set Y
+Y <- otu[,colnames(otu) %in% colnames(X)]
+
+# Specify priors
+upsilon <- ntaxa(ps)+3 
+Omega <- diag(ntaxa(ps))
+G <- cbind(diag(ntaxa(ps)-1), -1)
+Xi <- (upsilon-ntaxa(ps))*G%*%Omega%*%t(G)
+Theta <- matrix(0, ntaxa(ps)-1, nrow(X))
+Gamma <- diag(nrow(X))
+
+## prior checking
+priors <- pibble(NULL, X, upsilon, Theta, Gamma, Xi)  
+#print(priors)
+
+## convert to CLR
+priors <- to_clr(priors)  
+
+# set names
+names_covariates(priors) <- rownames(X)
+
+priors$Y <- Y
+
+# refit with priors to get posterior
+posterior <- refit(priors, optim_method="lbfgs", n_samples = 5000)
+
+# convert to CLR
+posterior <- to_clr(posterior)
+
+# name the posterior categories (taxa)
+names_categories(posterior) <- taxa_names(ps)
+params = summary(posterior, pars = "Lambda")
+
+# get sigs
+sig.params <- params$Lambda %>% 
+  filter(covariate == "fbg.stand") %>% 
+  filter(sign(p2.5) == sign(p97.5)) # 7
+
+# save results
+glucpar <- params
+save(glucpar, file = "results/fido-glucose-params.RData")
+
+# get r2
+quantile(r2(posterior, covariate = 2),
+         probs = c(0.025, 0.5, 0.975)
+)
+
+
+## ----- BY PAPER ----
+
+### FORSLUND 2015
+### NO PAIRED SAMPLES
+
+## subset metadata and OTU table
+ps <- psf %>% 
+  ps_filter(paper == "forslund2015") 
+
+df <- data.frame(metadata) %>% 
+  filter(paper == "forslund2015") %>% 
+  # drop NAs
+  drop_na(fasting.glucose.mg.dL) %>% 
+  # standardize fbg
+  mutate(fbg.stand = (fasting.glucose.mg.dL - mean(fasting.glucose.mg.dL) ) / sd(fasting.glucose.mg.dL)) %>% 
+  dplyr::select(fbg.stand)
+
+## run 
+fors <- myFIDO(ps, df, covariates.include = 2)
+
+### KARLSSON
+
+## subset metadata and OTU table
+ps <- psf %>% 
+  ps_filter(paper == "karlsson2013") 
+
+df <- data.frame(metadata) %>% 
+  filter(paper == "karlsson2013") %>% 
+  drop_na(fasting.glucose.mg.dL) %>% 
+  # standardize fbg
+  mutate(fbg.stand = (fasting.glucose.mg.dL - mean(fasting.glucose.mg.dL) ) / sd(fasting.glucose.mg.dL)) %>% 
+  dplyr::select(metformin, fbg.stand)
+
+karl <- myFIDO(ps, df, get_r2 = FALSE)
+
+### LECHATLIER 
+
+ps <- psf %>% 
+  ps_filter(paper == "lechatlier2013") 
+
+df <- data.frame(metadata) %>% 
+  filter(paper == "lechatlier2013") %>% 
+  drop_na(fasting.glucose.mg.dL) %>% 
+  # standardize fbg
+  mutate(fbg.stand = (fasting.glucose.mg.dL - mean(fasting.glucose.mg.dL) ) / sd(fasting.glucose.mg.dL)) %>% 
+  dplyr::select(fbg.stand)
+
+lech <- myFIDO(ps, df, get_r2 = FALSE)
+
+### LI 2014
+
+ps <- psf %>% 
+  ps_filter(paper == "li2014") 
+
+df <- data.frame(metadata) %>% 
+  filter(paper == "li2014") %>% 
+  drop_na(fasting.glucose.mg.dL) %>% 
+  # standardize fbg
+  mutate(fbg.stand = (fasting.glucose.mg.dL - mean(fasting.glucose.mg.dL) ) / sd(fasting.glucose.mg.dL)) %>% 
+  dplyr::select(fbg.stand, metformin)
+
+li <- myFIDO(ps, df, get_r2 = FALSE)
+
+
+### QIN 2012
+ps <- psf %>% 
+  ps_filter(paper == "qin2012") 
+
+df <- data.frame(metadata) %>% 
+  filter(paper == "qin2012") %>% 
+  drop_na(fasting.glucose.mg.dL) %>% 
+  # standardize fbg
+  mutate(fbg.stand = (fasting.glucose.mg.dL - mean(fasting.glucose.mg.dL) ) / sd(fasting.glucose.mg.dL)) %>% 
+  dplyr::select(fbg.stand, metformin)
+
+qin <- myFIDO(ps, df, get_r2 = FALSE)
+
+## SUN 2018 
+
+ps <- psf %>% 
+  ps_filter(paper == "sun2018") 
+
+df <- data.frame(metadata) %>% 
+  filter(paper == "sun2018") %>% 
+  ### GET ONLY BEFORE METFORMIN 
+  filter(metformin == "no.metformin") %>% 
+  drop_na(fasting.glucose.mg.dL) %>% 
+  # standardize fbg
+  mutate(fbg.stand = (fasting.glucose.mg.dL - mean(fasting.glucose.mg.dL) ) / sd(fasting.glucose.mg.dL)) %>% 
+  dplyr::select(fbg.stand)
+
+sun <- myFIDO(ps, df, get_r2 = FALSE)
+
+## ---- save all ----
+
+## add together
+all_pars <- rbind(fors %>% mutate(Study = "Forslund 2014"),
+                  karl %>% mutate(Study = "Karlsson 2013"),
+                  lech %>% mutate(Study = "LeChatlier 2013"),
+                  li %>% mutate(Study = "Li 2014"),
+                  qin %>% mutate(Study = "Qin 2012"),
+                  sun %>% mutate(Study = "Sun 2018"))
+
+save(all_pars, file = "results/fido-glucose-bypaper-params.RData")
+
+## ---- FILTER ----
+
+
+## the significant coords from global comparison
+sigs <- sig.params$coord # 7
+
+## filter the full dataset to only significant taxa
+bypaper <- all_pars %>% 
+  filter(coord %in% sigs) %>% 
+  # get only insulin
+  filter(covariate == "fbg.stand") %>% 
+  mutate(Genus = str_remove(coord, "clr_"))
+
+### THRESHOLD: there were 6 studies in the comparison,
+# so consider significant if 4 have same direction and have magnitude over 0.1
+
+
+taxon <- unique(bypaper$Genus)
+outdf <- data.frame()
+for(i in 1:length(taxon)) {
+  
+  # subset
+  sub <- bypaper %>% filter(Genus == taxon[i])
+  outdf[i, "Genus"] <- taxon[i]
+  
+  # summarize effect size direction (sign of mean)
+  
+  if(length(which(sign(sub$mean) == 1)) >= 4 |
+     length(which(sign(sub$mean) == -1)) >= 4) {
+    
+    # create new column in outdf
+    outdf[i, "signs"] <- TRUE
+  } else { outdf[i, "signs"] <- FALSE}
+  
+  
+  # summarize strength of effect size
+  if(length(which(abs(sub$mean) > 0.1)) >= 4) {
+    
+    outdf[i, "size"] <- TRUE
+  } else { outdf[i, "size"] <- FALSE}
+  
+}
+
+outdf
+
+## save taxa for new plot
+mod1 <- outdf %>% 
+  filter(signs == TRUE & size == TRUE) # 2
+
+
